dad6a5736b39c41a75ccb3267e5a7bbb2814892d
SipengWang <wangsipeng@zju.edu.cn>
2020-06-02 14:26:22 +0800

    add run.bash

---
+++ run.bash
@@ -0,0 +1,7 @@
+. ~/catkin_ws/Firmware/Tools/setup_gazebo.bash ~/catkin_ws/Firmware ~/catkin_ws/Firmware/build/px4_sitl_default
+source ~/catkin_ws/avoidance/devel/setup.bash
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:~/catkin_ws/Firmware
+export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:~/catkin_ws/avoidance/src/avoidance/avoidance/sim/models:~/catkin_ws/avoidance/src/avoidance/avoidance/sim/worlds
+
+
+roslaunch local_planner local_planner_stereo.launch
